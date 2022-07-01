0a1aee180fe60c86da2c2ea02ce8af3913e759c6
EtaJx <medianeras57@gmail.com>
2022-07-02 00:20:54 +0800

    Update sh

--- scripts/updateRepo.sh
+++ scripts/updateRepo.sh
@@ -1,4 +1,15 @@
+echo "updating repo"
 cd ~/nextjs-personal
 git fetch --all
 git pull origin master
+echo "update repo successfully"
+# delete old node_modules folders & force re-install
+echo "updating node_modules"
+rm -rf node_modules && yarn install
+echo "update node_modules successfully"
+# delete old article list cached file
+rm data.json
+# then restart service re-pull fresh list
+echo "restating service"
 pm2 restart nextjs-personal
+echo "restarted"
